98ab68c04da57f1ca10fee849d3880ba0d38ef66
tahirac <tahirac@HEDAYET.home>
2017-11-27 00:45:15 -0500

    changes1

--- Web object/src/webobjectidentification/Webobjectidentification.java
+++ Web object/src/webobjectidentification/Webobjectidentification.java
@@ -5,7 +5,7 @@ public class Webobjectidentification {
 	public static void main(String[] args) 
 	{
 	
-System.out.println("marcury site is a ");
+System.out.println("marcury bpuaikjdcbkjdcbksdjbv;ldcvb site is a ");
 	}
 
 }
